b6fa0eaac62f494e6dd7f4dec4a8477767fb0384
rashid <rashid-11-26@mail.ru>
2022-12-27 10:38:05 +0600

    changes

--- src/app/Pages/main/main.component.ts
+++ src/app/Pages/main/main.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ISlide {
+  image: string;
+  active?: boolean;
+}
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  slideInterval: any;
+
+  slides: ISlide[] = [
+    { image: './assets/images/nfts/image1.png', active: true },
+    { image: './assets/images/nfts/image1.png', active: false },
+    { image: './assets/images/nfts/image1.png', active: false },
+    { image: './assets/images/nfts/image1.png', active: false },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.slideInterval = setInterval(() => {
+      let image = this.slides.filter(s => s.active)[0];
+      if (image) {
+        let index = this.slides.indexOf(image);
+        if (index === this.slides.length - 1) {
+          index = 0;
+        }
+        this.slides.forEach(s => s.active = false);
+        this.slides[index + 1].active = true;
+      }
+    }, 3000);
   }
 
 }
