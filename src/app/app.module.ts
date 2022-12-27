f493f49b3d9910f2e39d50704df4402c1e27d09a
rashid <rashid-11-26@mail.ru>
2022-12-27 12:10:16 +0600

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './Pages/main/main.component';
+import { SocialsComponent } from './components/socials/socials.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SocialsComponent
   ],
   imports: [
     BrowserModule,
